8b6f924674884ba2ca18489ab95105558c560fd7
SRIRAM <srirammurugesan1807@gmail.com>
2025-09-15 08:09:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
 import React from "react";
-import './../styles/App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import PostsPage from "./components/PostsPage";
+import UsersPage from "./components/UsersPage";
+import NotificationsPage from "./components/NotificationsPage";
+import PostDetails from "./components/PostDetails";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <h1>GenZ</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<PostsPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+      </Routes>
     </div>
-  )
+  );
 }
-
-export default App
